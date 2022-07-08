8ec57d5083beb57301e091b0025e616884ecebd9
pativilaka <p.vilaka@gmail.com>
2022-07-08 11:04:30 -0300

    Inclusão de exercício Java na IDE Intellij

---
+++ primeiro-programa-java-intellij/src/ide/PrimeiroProgramaJava.java
@@ -0,0 +1,17 @@
+package ide;
+
+import ide.model.Gato;
+
+public class PrimeiroProgramaJava {
+    public static void main(String[] args) {
+
+       /* int a = 5;
+        int b = 2;
+        System.out.println("Hello world " + (a+b));*/
+
+        Gato gato = new Gato();
+
+        System.out.println(gato);
+
+    }
+}
